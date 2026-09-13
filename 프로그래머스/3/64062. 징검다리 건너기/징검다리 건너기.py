d3353a5e082a69a7a4c8bb832c232e8deca0c51b
Hyehyeon Kim <63723227+K-Hyeon@users.noreply.github.com>
2026-09-13 20:04:45 +0900

    [level 3] Title: 징검다리 건너기, Time: 61.21 ms, Memory: 19.7 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,26 +1,25 @@
-# 시간초과
+from collections import deque
+
+def solution(stones, k):
+    dq = deque()
+    answer = 1e9
+
+    for i in range(len(stones)):
+        while dq and stones[dq[-1]] <= stones[i]: dq.pop()
+        dq.append(i)
+        if dq[0] <= i - k: dq.popleft()
+        if i >= k - 1:
+            max_value = stones[dq[0]]
+            answer = min(answer, max_value)
+    return answer
+
+
 """
+# 시간 초과 코드
 def solution(stones, k):
     answer = 1e9
     for i in range(0, len(stones) - k + 1):
         max_value = max(stones[i : i+k])
-        if max_value < answer : answer = max_value
-        print(max_value, stones[i : i+k])
+        if max_value < answer:answer = max_value
     return answer
 """
-
-def solution(stones, k):
-    left = 0
-    right = max(stones)+1
-
-    def compare(mid):
-        return any(len(i)>=k for i in "".join(["T" if i>mid else "F" for i in stones]).split("T"))
-
-    while left<right:
-        mid = (left+right)//2
-        if compare(mid):
-            right = mid
-        else:
-            left = mid + 1
-
-    return right
